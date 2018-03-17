14e811be6dfb1581be663bcfaddc2646a5839c6d
sanchaman <sanchamansubba16@gmail.com>
2018-03-17 22:53:55 +0530

    connection

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image =require('./controllers/image');
 const db =	knex({
 	    client: 'pg',
 	    connection: {
-		    host : 'postgresql-concave-35186',
-		    user : 'sanchaman',
-		    password : '',
-		    database : 'smart-brain'
+		    host : process.env.DATABASE_URL,
+		    ssl:true
+		    
         }
 });
 
