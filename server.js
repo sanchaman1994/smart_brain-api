b7270b39def84e8efc109d3b8087984920c11894
sanchaman <sanchamansubba16@gmail.com>
2018-03-17 22:49:25 +0530

    updationg database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image =require('./controllers/image');
 const db =	knex({
 	    client: 'pg',
 	    connection: {
-		    host : '127.0.0.1',
+		    host : 'postgresql-concave-35186',
 		    user : 'sanchaman',
 		    password : '',
 		    database : 'smart-brain'
