104e4e6b1a7b16fb4c722e3037894b32af9bca70
sanchaman <sanchamansubba16@gmail.com>
2018-03-17 23:50:42 +0530

    shiwani

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.post('/imageUrl',(req,res)=>{ image.handleApiCall(req,res)});
 
 
 
-app.listen(process.env.PORT || 3000, () => 
+app.listen(process.env.PORT || 3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
+
 });
